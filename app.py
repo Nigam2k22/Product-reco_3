b89a23409c4bc4e99cf5bc6c9399664d006ca3ad
Nigam2k22 <96674821+Nigam2k22@users.noreply.github.com>
2022-04-13 13:18:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,31 @@
+from email import header
+from operator import index
+from flask import Flask, request, render_template, jsonify
+from model import SentimentRecommenderModel
+
+
+app = Flask(__name__)
+
+sentiment_model = SentimentRecommenderModel()
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def prediction():
+    user = request.form['userName']
+    user = user.lower()
+    items = sentiment_model.getSentimentRecommendations(user)
+
+    if(not(items is None)):
+        print(f"retrieving items....{len(items)}")
+        print(items)
+        return render_template("index.html", column_names=items.columns.values, row_data=list(items.values.tolist()), zip=zip)
+    else:
+        return render_template("index.html", message="User Name doesn't exists, No product recommendations at this point of time!")
+
+if __name__ == '__main__':
+    app.run()
